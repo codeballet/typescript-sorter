f33e98b806a74566e140ec6a95efb0c98e8f5653
Johan <johan@johan-mac.local>
2020-09-09 16:15:52 +0800

    Feature: abstract classes

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,19 +1,24 @@
-export interface Sortable {
-  length: number;
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
+// No need for the interface
+// classes coupled together through abstract classes
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+// export interface Sortable {
+//   length: number;
+//   compare(leftIndex: number, rightIndex: number): boolean;
+//   swap(leftIndex: number, rightIndex: number): void;
+// }
+
+export abstract class Sorter {
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
